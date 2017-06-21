ad9e914319e89ea1463861b2b0232f0b3c8caff2
Russell Simmons <rws10@live.com>
2017-06-21 16:52:23 -0400

    Started the Media Service activity

--- app/src/main/java/edu/fsu/cs/mobile/homework4/MyMediaService.java
+++ app/src/main/java/edu/fsu/cs/mobile/homework4/MyMediaService.java
@@ -2,6 +2,9 @@ package edu.fsu.cs.mobile.homework4;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.IBinder;
 
 public class MyMediaService extends Service {
@@ -23,5 +26,11 @@ public class MyMediaService extends Service {
     public void loadPlayer(String fileuri) {
         // TODO prepare MediaPlayer with fileuri
         // possibly activate the play button here.
+        Uri songUri = Uri.parse(fileuri);
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        //mediaPlayer.setDataSource(getApplicationContext(), songUri);
+        //mediaPlayer.prepare();
+        mediaPlayer.start();
     }
 }
